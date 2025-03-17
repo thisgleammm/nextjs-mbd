90a2109fea0a282500ca8d11309deafdc298c234
thisgleammm <thisgleam@gmail.com>
2025-03-17 20:20:11 +0700

    Fix lib/prisma.ts Failed To Commpile

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,9 @@
 import { PrismaClient } from "@prisma/client";
-import { withOptimize } from "@prisma/extension-optimize";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const prisma =
-  globalThis.prisma ||
-  new PrismaClient().$extends(
-    withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY })
-  );
+export const prisma = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
